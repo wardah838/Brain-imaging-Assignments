7d4988e79de3c972ad81f88da9edd46b78bd32e4
wardah838 <78582584+wardah838@users.noreply.github.com>
2021-02-28 22:56:01 +0530

    Reading the image

--- Assignment 2: Image processing/Codes.py
+++ Assignment 2: Image processing/Codes.py
@@ -8,3 +8,13 @@ ls          #check for the file
 
 #activate python
 python
+
+
+#import matplotlib.pyplot 
+import matplotlib.pyplot as plt
+from scipy import misc, ndimage
+
+#reading the image of the brain
+brain_image=plt.imread("Brain_image.jpg")
+plt.imshow(brain_image)
+plt.show()
