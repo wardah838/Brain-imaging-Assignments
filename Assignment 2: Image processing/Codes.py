e51df7ed55e40b12ed6987e6c1b0cb96f8554cd6
wardah838 <78582584+wardah838@users.noreply.github.com>
2021-02-28 22:54:28 +0530

    Create environment

---
+++ Assignment 2: Image processing/Codes.py
@@ -0,0 +1,10 @@
+#activate image processing 
+conda activate image_process
+
+#navigate to the folder which has the image
+cd /mnt/c/Users/warda/Downloads
+ls          #check for the file
+
+
+#activate python
+python
