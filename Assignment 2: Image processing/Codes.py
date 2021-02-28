ae9f06ec081ce60a24142263eaf05e899235fdc5
wardah838 <78582584+wardah838@users.noreply.github.com>
2021-02-28 23:06:34 +0530

    sigma = 30 Gaussian filtering

--- Assignment 2: Image processing/Codes.py
+++ Assignment 2: Image processing/Codes.py
@@ -57,3 +57,11 @@ plt.imshow(brain_image_sigma20, cmap='Greys_r')
 plt.show()
 plt.hist(brain_image_sigma20, cmap='Greys_r')
 plt.show()
+
+
+#sigma=30 Gaussian filter and histogram
+brain_image_sigma30=ndimage.gaussian_filter(brain_image, sigma=30)
+plt.imshow(brain_image_sigma30, cmap='Greys_r')
+plt.show()
+plt.hist(brain_image_sigma30, cmap='Greys_r')
+plt.show()
