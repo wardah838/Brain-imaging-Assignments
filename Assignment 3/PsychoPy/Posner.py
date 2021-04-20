86198cd3a7a86ca6e2ba6b24bca9bd1fbaa35689
wardah838 <78582584+wardah838@users.noreply.github.com>
2021-04-21 01:39:41 +0530

    Update Posner.py

--- Assignment 3/PsychoPy/Posner.py
+++ Assignment 3/PsychoPy/Posner.py
@@ -445,6 +445,113 @@ for thisTrial in trials:
         if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
             win.flip()
     
+     #Ending Routine "trial"
+    for thisComponent in trialComponents:
+        if hasattr(thisComponent, "setAutoDraw"):
+            thisComponent.setAutoDraw(False)
+    trials.addData('fixation.started', fixation.tStartRefresh)
+    trials.addData('fixation.stopped', fixation.tStopRefresh)
+    trials.addData('cue.started', cue.tStartRefresh)
+    trials.addData('cue.stopped', cue.tStopRefresh)
+    trials.addData('target.started', target.tStartRefresh)
+    trials.addData('target.stopped', target.tStopRefresh)
+    # store data for trials (TrialHandler)
+    if len(mouse.x): trials.addData('mouse.x', mouse.x[0])
+    if len(mouse.y): trials.addData('mouse.y', mouse.y[0])
+    if len(mouse.leftButton): trials.addData('mouse.leftButton', mouse.leftButton[0])
+    if len(mouse.midButton): trials.addData('mouse.midButton', mouse.midButton[0])
+    if len(mouse.rightButton): trials.addData('mouse.rightButton', mouse.rightButton[0])
+    if len(mouse.time): trials.addData('mouse.time', mouse.time[0])
+    if len(mouse.clicked_name): trials.addData('mouse.clicked_name', mouse.clicked_name[0])
+    trials.addData('mouse.started', mouse.tStart)
+    trials.addData('mouse.stopped', mouse.tStop)
+    # check responses
+    if keyPress.keys in ['', [], None]:  # No response was made
+        keyPress.keys = None
+        # was no response the correct answer?!
+        if str(corrAns).lower() == 'none':
+           keyPress.corr = 1;  # correct non-response
+        else:
+           keyPress.corr = 0;  # failed to respond (incorrectly)
+    # store data for trials (TrialHandler)
+    trials.addData('keyPress.keys',keyPress.keys)
+    trials.addData('keyPress.corr', keyPress.corr)
+    if keyPress.keys != None:  # we had a response
+        trials.addData('keyPress.rt', keyPress.rt)
+    trials.addData('keyPress.started', keyPress.tStartRefresh)
+    trials.addData('keyPress.stopped', keyPress.tStopRefresh)
+    
+   #Prepare to start Routine "BlankScreen5"
+    continueRoutine = True
+    routineTimer.add(0.600000)
+    # update component parameters for each repeat
+    # keep track of which components have finished
+    BlankScreen5Components = [text]
+    for thisComponent in BlankScreen5Components:
+        thisComponent.tStart = None
+        thisComponent.tStop = None
+        thisComponent.tStartRefresh = None
+        thisComponent.tStopRefresh = None
+        if hasattr(thisComponent, 'status'):
+            thisComponent.status = NOT_STARTED
+    # reset timers
+    t = 0
+    _timeToFirstFrame = win.getFutureFlipTime(clock="now")
+    BlankScreen5Clock.reset(-_timeToFirstFrame)  # t0 is time of first possible flip
+    frameN = -1
+    
+    # Run Routine "BlankScreen5"
+    while continueRoutine and routineTimer.getTime() > 0:
+        # get current time
+        t = BlankScreen5Clock.getTime()
+        tThisFlip = win.getFutureFlipTime(clock=BlankScreen5Clock)
+        tThisFlipGlobal = win.getFutureFlipTime(clock=None)
+        frameN = frameN + 1  # number of completed frames (so 0 is the first frame)
+        # update/draw components on each frame
+        
+        # *text* updates
+        if text.status == NOT_STARTED and tThisFlip >= 0.0-frameTolerance:
+            # keep track of start time/frame for later
+            text.frameNStart = frameN  # exact frame index
+            text.tStart = t  # local t and not account for scr refresh
+            text.tStartRefresh = tThisFlipGlobal  # on global time
+            win.timeOnFlip(text, 'tStartRefresh')  # time at next scr refresh
+            text.setAutoDraw(True)
+        if text.status == STARTED:
+            # is it time to stop? (based on global clock, using actual start)
+            if tThisFlipGlobal > text.tStartRefresh + 0.6-frameTolerance:
+                # keep track of stop time/frame for later
+                text.tStop = t  # not accounting for scr refresh
+                text.frameNStop = frameN  # exact frame index
+                win.timeOnFlip(text, 'tStopRefresh')  # time at next scr refresh
+                text.setAutoDraw(False)
+        
+        # check for quit (typically the Esc key)
+        if endExpNow or defaultKeyboard.getKeys(keyList=["escape"]):
+            core.quit()
+        
+        # check if all components have finished
+        if not continueRoutine:  # a component has requested a forced-end of Routine
+            break
+        continueRoutine = False  # will revert to True if at least one component still running
+        for thisComponent in BlankScreen5Components:
+            if hasattr(thisComponent, "status") and thisComponent.status != FINISHED:
+                continueRoutine = True
+                break  # at least one component has not yet finished
+        
+        # refresh the screen
+        if continueRoutine:  # don't flip if this routine is over or we'll get a blank screen
+            win.flip()
+    
+    # Ending Routine "BlankScreen"
+    for thisComponent in BlankScreen5Components:
+        if hasattr(thisComponent, "setAutoDraw"):
+            thisComponent.setAutoDraw(False)
+    trials.addData('text.started', text.tStartRefresh)
+    trials.addData('text.stopped', text.tStopRefresh)
+    thisExp.nextEntry()
+    
+# completed 2 repeats of 'trials'
         
 
 
