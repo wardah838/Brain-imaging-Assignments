cacfa19e641c7706ac6336061fc810e96a954b5f
wardah838 <78582584+wardah838@users.noreply.github.com>
2021-04-21 01:17:30 +0530

    Update Posner.py

--- Assignment 3/PsychoPy/Posner.py
+++ Assignment 3/PsychoPy/Posner.py
@@ -6,3 +6,13 @@ from psychopy import prefs
 from psychopy import sound, gui, visual, core, data, event, logging, clock
 from psychopy.constants import (NOT_STARTED, STARTED, PLAYING, PAUSED,
                                 STOPPED, FINISHED, PRESSED, RELEASED, FOREVER)
+
+#importing numpy
+import numpy as np  # whole numpy lib is available, prepend 'np.'
+from numpy import (sin, cos, tan, log, log10, pi, average,
+                   sqrt, std, deg2rad, rad2deg, linspace, asarray)
+from numpy.random import random, randint, normal, shuffle
+import os  # handy system and path functions
+import sys  # to get file system encoding
+
+from psychopy.hardware import keyboard
