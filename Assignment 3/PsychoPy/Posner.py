5dd9f1e93d756637a18baa039b43b0f68083f37b
wardah838 <78582584+wardah838@users.noreply.github.com>
2021-04-21 01:16:23 +0530

    Create Posner.py

---
+++ Assignment 3/PsychoPy/Posner.py
@@ -0,0 +1,8 @@
+#intializing 
+from __future__ import absolute_import, division
+
+from psychopy import locale_setup
+from psychopy import prefs
+from psychopy import sound, gui, visual, core, data, event, logging, clock
+from psychopy.constants import (NOT_STARTED, STARTED, PLAYING, PAUSED,
+                                STOPPED, FINISHED, PRESSED, RELEASED, FOREVER)
