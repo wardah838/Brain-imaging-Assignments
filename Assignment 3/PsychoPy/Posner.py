2844082a4b8843a31ae3af0aadee2450421f38bf
wardah838 <78582584+wardah838@users.noreply.github.com>
2021-04-21 01:20:58 +0530

    Update Posner.py

--- Assignment 3/PsychoPy/Posner.py
+++ Assignment 3/PsychoPy/Posner.py
@@ -7,8 +7,8 @@ from psychopy import sound, gui, visual, core, data, event, logging, clock
 from psychopy.constants import (NOT_STARTED, STARTED, PLAYING, PAUSED,
                                 STOPPED, FINISHED, PRESSED, RELEASED, FOREVER)
 
-#importing numpy
-import numpy as np  # whole numpy lib is available, prepend 'np.'
+#importing numpy, os, sys
+import numpy as np 
 from numpy import (sin, cos, tan, log, log10, pi, average,
                    sqrt, std, deg2rad, rad2deg, linspace, asarray)
 from numpy.random import random, randint, normal, shuffle
@@ -16,3 +16,20 @@ import os  # handy system and path functions
 import sys  # to get file system encoding
 
 from psychopy.hardware import keyboard
+
+# Ensure that relative paths start from the same directory as this script
+_thisDir = os.path.dirname(os.path.abspath(__file__))
+os.chdir(_thisDir)
+
+# Storing information about the experimental sessions
+psychopyVersion = '2020.2.10'
+expName = 'posner v1'  # from the Builder filename that created this script
+expInfo = {'participant': '', 'session': '001'}
+dlg = gui.DlgFromDict(dictionary=expInfo, sortKeys=False, title=expName)
+if dlg.OK == False:
+    core.quit()  # user pressed cancel
+expInfo['date'] = data.getDateStr()  # add a simple timestamp
+expInfo['expName'] = expName
+expInfo['psychopyVersion'] = psychopyVersion
+
+
